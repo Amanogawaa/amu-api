6d1df9a6ae8cad5bfaa65c7f8eefd21508b2f596
Amanogawaa <dommolino28@gmail.com>
2025-08-01 12:31:05 +0800

    add express

---
+++ src/db/schema.ts
@@ -0,0 +1,11 @@
+import type { InferModel, InferSelectModel } from "drizzle-orm";
+import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+
+export const usersTable = pgTable("users", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: varchar({ length: 255 }).notNull(),
+  age: integer().notNull(),
+  email: varchar({ length: 255 }).notNull().unique(),
+});
+
+export type User = InferSelectModel<typeof usersTable>;
